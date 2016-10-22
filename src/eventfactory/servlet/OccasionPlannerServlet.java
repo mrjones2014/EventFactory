1dcf98e2a19e7488b9bf46bea630a78586727647
Daniel Reightneour <dreightn@ycp.edu>
2016-10-22 08:07:45 -0400

    Rename to Servlet

--- src/eventfactory/servlet/OccasionPlannerServlet.java
+++ src/eventfactory/servlet/OccasionPlannerServlet.java
@@ -10,15 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import eventfactory.model.Occasion;
 
-public class OccasionPlanner extends HttpServlet {
+public class OccasionPlannerServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-		
-		
-		
 		req.getRequestDispatcher("/_view/occasionPlanner.jsp").forward(req, resp);
 	}
 	
