b21f661a756da3df201fd2892f9e054b69859e33
Robert Jones <rjones38@ycp.edu>
2016-10-22 03:21:19 -0400

    just got rid of an error for now in tester class

--- junit/testerpackage/EventTester.java
+++ junit/testerpackage/EventTester.java
@@ -4,7 +4,7 @@ import eventfactory.main.Event;
 import junit.framework.TestCase;
 
 public class EventTester extends TestCase {
-	Event e = new Event("");
+	//Event e = new Event("");
 	public EventTester(){
 		
 	}
