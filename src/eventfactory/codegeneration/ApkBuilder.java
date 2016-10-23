861808c667803e41bf39f5a9ed6fbd6bba4f95df
Mat Jones <mjones44@ycp.edu>
2016-10-22 21:32:24 -0400

    Added class to generate signed APK file

---
+++ src/eventfactory/codegeneration/ApkBuilder.java
@@ -0,0 +1,50 @@
+package eventfactory.codegeneration;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+
+import kellinwood.security.zipsigner.ZipSigner;
+
+public class ApkBuilder {
+	
+	public void generateAndSignApk(String appName) throws Exception{
+		Runtime rt = Runtime.getRuntime();
+		String[] commands = {"system.exe","-get t"};
+		Process proc = rt.exec(commands);
+
+		BufferedReader stdInput = new BufferedReader(new 
+		     InputStreamReader(proc.getInputStream()));
+
+		BufferedReader stdError = new BufferedReader(new 
+		     InputStreamReader(proc.getErrorStream()));
+		String accum = "";
+		String s = null;
+		while ((s = stdInput.readLine()) != null) {
+		    accum += s + "\n";
+		}
+
+		if(!accum.contains("SUCCESSFUL") && !accum.contains("successful")) throw new Exception("Build failed; trace: \n" + accum);
+		
+		try{ signAPK(appName); } catch(Throwable t) { t.printStackTrace(); }
+	}
+	
+	private void signAPK(String appName) throws ClassNotFoundException, IllegalAccessException, InstantiationException, IOException, GeneralSecurityException {
+    	String apkDir = this.getClass().getResource("/apk_templates/$app_name/app/build/outputs/apk/").getPath();
+        String generatedApkPath = apkDir + appName + ".apk";
+
+        String signedApkPath = apkDir + appName + "-signed.apk";
+
+        ZipSigner zipSigner = null;
+        zipSigner = new ZipSigner();
+
+        zipSigner.setKeymode(ZipSigner.MODE_AUTO);
+
+        zipSigner.signZip(generatedApkPath, signedApkPath);
+
+        // After signing apk , delete unsigned apk
+        new File(generatedApkPath).delete();
+	}
+}
