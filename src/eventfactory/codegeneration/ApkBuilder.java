b1e99b4ceb8e08997d421a58554dfdbe36111e9e
Mat Jones <mjones44@ycp.edu>
2016-10-23 08:25:26 -0400

    working on ApkBuilder

--- src/eventfactory/codegeneration/ApkBuilder.java
+++ src/eventfactory/codegeneration/ApkBuilder.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.security.GeneralSecurityException;
 
 import kellinwood.security.zipsigner.ZipSigner;
@@ -20,21 +21,29 @@ public class ApkBuilder {
 	
 	public void generateAndSignApk(String appName) throws Exception{
 		Runtime rt = Runtime.getRuntime();
-		String gradlewBat = this.getClass().getResource("/apk_templates/$app_name/gradlew.bat").getPath();
+		String gradlewBat = "apk_templates/$app_name/gradlew.bat";
 		
-		String[] commands = {gradlewBat + " assembleRelease"};
-		Process proc = rt.exec(commands);
+		String[] commands = {"gradlew.bat", "assembleRelease"};
+		//Process proc = rt.exec(commands, null, new File(new File("apk_templates/$app_name/").getAbsolutePath()));
+		Process proc = rt.exec(new String[] {"cmd.exe", "/c", "\"apk_templates/$app_name/gradlew.bat\""});
 
 		BufferedReader stdInput = new BufferedReader(new 
 		     InputStreamReader(proc.getInputStream()));
+		BufferedReader stdError = new BufferedReader(new 
+			     InputStreamReader(proc.getErrorStream()));
 
 		String accum = "";
 		String s = null;
 		while ((s = stdInput.readLine()) != null) {
 		    accum += s + "\n";
 		}
+		
+		String accum2 = "";
+		while ((s = stdError.readLine()) != null) {
+		    accum += s + "\n";
+		}
 
-		if(!accum.contains("SUCCESSFUL") && !accum.contains("successful")) throw new Exception("Build failed; trace: \n" + accum);
+		if(!accum.contains("SUCCESSFUL") && !accum.contains("successful")) throw new Exception("Build failed; trace: \n" + accum + "\n" + accum2);
 		
 		try{ signAPK(appName); } catch(Throwable t) { t.printStackTrace(); }
 		
